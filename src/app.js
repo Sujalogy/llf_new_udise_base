007ba32d74e4e0f72de3914a70d23417d613295a
sujal <thesujalogy@gmail.com>
2025-12-18 11:16:10 +0530

    adding cors changes

--- src/app.js
+++ src/app.js
@@ -7,7 +7,29 @@ const { getYears } = require("./controllers/locationController");
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  "https://school-directory.llf.org.in",
+  "http://localhost:8080"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow server-to-server & tools like Postman
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 
 // Routes
